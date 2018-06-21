e3db5f3aca0de22768e83366c952c80c47b0397c
Isaac Collier <icolp@allstate.com>
2018-06-21 09:09:42 -0700

    [#157610871] - Add allow invalid user to config environment struct
    Signed-off-by: Isaac Collier <icolp@allstate.com>

--- structs/environment.go
+++ structs/environment.go
@@ -2,12 +2,13 @@ package structs
 
 // Environment is representation of a single environment configuration.
 type Environment struct {
-	Name            string
-	Domain          string
-	Foundations     []string `yaml:",flow"`
-	Authenticate    bool
-	SkipSSL         bool `yaml:"skip_ssl"`
-	Instances       uint16
-	DisableRollback bool                   `yaml:"rollback_disabled"`
-	CustomParams    map[string]interface{} `yaml:"custom_params"`
+	Name             string
+	Domain           string
+	Foundations      []string `yaml:",flow"`
+	Authenticate     bool
+	SkipSSL          bool `yaml:"skip_ssl"`
+	Instances        uint16
+	DisableRollback  bool                   `yaml:"rollback_disabled"`
+	CustomParams     map[string]interface{} `yaml:"custom_params"`
+	AllowInvalidUser bool                   `yaml:"allow_invalid_users"`
 }
